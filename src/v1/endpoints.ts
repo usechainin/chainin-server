b731d3a4bab2b2a69684ea6cdcbdef2c8f9494ea
Dann Wee <dannweezj@gmail.com>
2023-12-02 23:55:40 +0700

    deleted company, education and school

--- src/v1/endpoints.ts
+++ src/v1/endpoints.ts
@@ -1,10 +1,7 @@
 import { Express, Request, Response } from "express";
 import middleware from "./middleware";
 import userHandler from "./handlers/userHandler";
-import educationHandler from "./handlers/educationHandler";
 import handleRootRequest from "./handlers/rootHandler";
-import schoolHandler from "./handlers/schoolHandler";
-import companyHandler from "./handlers/companyHandler";
 import organisationHandler from "./handlers/organisationHandler";
 
 type Endpoint = {
@@ -121,79 +118,4 @@ endpoints.deleteOrganisationByOrganisationId = {
   description: "delete organisation by organisation id",
 };
 
-/////////////////////////////////////////
-/////////////// EDUCATION ///////////////
-/////////////////////////////////////////
-
-endpoints.createEducation = {
-  url: "/v1/education",
-  method: "post",
-  middleware: [middleware.checkWhitelistedIpAddress],
-  handler: educationHandler.create,
-  description: "create education",
-};
-
-endpoints.readEducationByWalletAddress = {
-  url: "/v1/education/:wallet_address",
-  method: "get",
-  middleware: [],
-  handler: educationHandler.readByWalletAddress,
-  description: "read education by wallet address",
-};
-
-/////////////////////////////////////////
-//////////////// SCHOOL /////////////////
-/////////////////////////////////////////
-
-endpoints.createSchool = {
-  url: "/v1/school",
-  method: "post",
-  middleware: [middleware.checkWhitelistedIpAddress],
-  handler: schoolHandler.create,
-  description: "create school",
-};
-
-endpoints.readSchool = {
-  url: "/v1/school",
-  method: "get",
-  middleware: [],
-  handler: schoolHandler.read,
-  description: "read all schools",
-};
-
-endpoints.readSchoolBySchoolName = {
-  url: "/v1/school/:school_name",
-  method: "get",
-  middleware: [],
-  handler: schoolHandler.readBySchoolName,
-  description: "read school by school name",
-};
-
-/////////////////////////////////////////
-/////////////// COMPANY /////////////////
-/////////////////////////////////////////
-
-endpoints.createCompany = {
-  url: "/v1/company",
-  method: "post",
-  middleware: [middleware.checkWhitelistedIpAddress],
-  handler: companyHandler.create,
-  description: "create company",
-};
-
-endpoints.readCompany = {
-  url: "/v1/company",
-  method: "get",
-  middleware: [],
-  handler: companyHandler.read,
-  description: "read all companies",
-};
-
-endpoints.readCompanyByCompanyName = {
-  url: "/v1/company/:company_name",
-  method: "get",
-  middleware: [],
-  handler: companyHandler.readByCompanyName,
-  description: "read company by company name",
-};
 export default endpoints;
